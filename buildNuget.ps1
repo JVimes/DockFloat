0636f2db52e13633a96582d84b88b39637d257b6
John Burak <john.burak@gmail.com>
2017-05-23 21:02:49 -0600

    Build nuget package from Release build

--- buildNuget.ps1
+++ buildNuget.ps1
@@ -1,10 +1,11 @@
 param($SolutionDir, $ProjectPath, $ConfigurationName)
+#Write-Host "[$SolutionDir], [$ProjectPath], [$ConfigurationName]"
 
 if($ConfigurationName -ne "Release"){ return }
 
 $SolutionDir = $SolutionDir.Trim()
 $ProjectPath = $ProjectPath.Trim()
 
-Write-Host "Building NuGet package $ConfigurationName"
+Write-Host "Building NuGet package"
 $nuget = "$SolutionDir\packages\NuGet.CommandLine.4.*\tools\NuGet.exe"
-& $nuget pack $ProjectPath
+& $nuget pack $ProjectPath -Prop Configuration=$ConfigurationName
